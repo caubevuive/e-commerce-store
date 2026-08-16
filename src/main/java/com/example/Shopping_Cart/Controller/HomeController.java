f5d0920fbf39d0d963b4a416c28b41fe5a4f8e07
vuanhquan <anhquanvu44@gmail.com>
2026-08-17 02:48:22 +0700

    feat: setup shopping cart project and base layout

---
+++ src/main/java/com/example/Shopping_Cart/Controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.Shopping_Cart.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		System.out.println(">>> Home page được gọi");
+		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+}
