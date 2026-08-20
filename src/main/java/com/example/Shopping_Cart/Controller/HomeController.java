272e6d5583ea38c6951859e2f858a9a6b2c8f4a7
vuanhquan <anhquanvu44@gmail.com>
2026-08-20 20:34:49 +0700

    feat: add product listing and detail pages

--- src/main/java/com/example/Shopping_Cart/Controller/HomeController.java
+++ src/main/java/com/example/Shopping_Cart/Controller/HomeController.java
@@ -21,4 +21,14 @@ public class HomeController {
 	public String register() {
 		return "register";
 	}
+
+	@GetMapping("/products")
+	public String products() {
+		return "product";
+	}
+
+	@GetMapping("/product")
+	public String product() {
+		return "view_product";
+	}
 }
